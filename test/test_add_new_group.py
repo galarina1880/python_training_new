217445b437c756019cdf1d657fae482b0303d775
Galarina <gmokrinskaya@gmail.com>
2019-11-08 15:18:51 +0300

    session extracted to separate file

--- test/test_add_new_group.py
+++ test/test_add_new_group.py
@@ -12,12 +12,12 @@ def app(request):
 
 
 def test_new_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.add_new_group(Group(name="gr1", header="header 1", footer="footer 1"))
-    app.logout()
+    app.session.logout()
 
 
 def test_new_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.add_new_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
