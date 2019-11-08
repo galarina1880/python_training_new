0cff1c3337a148f63144b44a8621cacfd6852239
Galarina <gmokrinskaya@gmail.com>
2019-11-08 16:42:54 +0300

    delete group test added

--- fixture/group.py
+++ fixture/group.py
@@ -27,3 +27,11 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.open_groups_page()
 
+    def del_first(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.open_groups_page()
